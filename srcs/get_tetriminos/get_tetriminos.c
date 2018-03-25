859cc8bb87c6da00d26c49629468dad7982d7668
lenox <maxime.kayumba@epitech.eu>
2018-03-25 17:10:42 +0200

    update get_tetriminos.c, put_tetriminos_list

--- srcs/get_tetriminos/get_tetriminos.c
+++ srcs/get_tetriminos/get_tetriminos.c
@@ -2,39 +2,19 @@
 
 char	*get_tetriminos(char *file)
 {
-	int		fd = 0;
-	char		*buf = NULL;
-	int		ret = 0;
-	struct stat	buf_size;
+	char	*lines;
+	char	*tmp;
+	char	*p;
+	int	fd;
 
-	CHECK_OPEN_FILE((fd = open(file, O_RDONLY)));
-	if (stat(file, &buf_size) == -1)
-		return (NULL);
-	MALLOC_CHECK((buf = malloc(sizeof(char) * buf_size.st_size + 1)));
-	CHECK_READ_FILE((ret = read(fd, buf, buf_size.st_size)));
-	buf[ret] = '\0';
-	if (close(fd) == -1)
-		return (NULL);
-	return (buf);
-}
-
-t_list	*put_tetriminos_list(char **lines)
-{
-	int		i;
-	char		**tetris;
-	t_tetris	*node;
-	t_list		*new;
-	t_list		*tmp;
-
-	i = -1;
-	tmp = NULL;
-	while (lines[++i])
+	tmp = ft_strnew(0);
+	p = tmp;
+	fd = open(file, O_RDONLY);
+	while (ft_gnl(fd, &lines))
 	{
-		tetris = ft_strsplit(lines[i], '\0');
-		node  = fill_node(tetris, (char) i + 65);
-		new = ft_lstnew(node, sizeof(node) + sizeof(tetris));
-		new->next = tmp;
-		tmp = new;
+		p = tmp;
+		tmp = ft_strjoin(tmp, lines);
+		ft_memdel((void **)&p);
 	}
-	return (NULL);
+	return (tmp);
 }
---
+++ srcs/get_tetriminos/put_tetriminos_list.c
@@ -0,0 +1,22 @@
+#include "fillit.h"
+
+t_list	*put_tetriminos_list(char **lines)
+{
+	int		i;
+	char		**tetris;
+	t_tetris	*node;
+	t_list		*new;
+	t_list		*tmp;
+
+	i = -1;
+	tmp = NULL;
+	while (lines[++i])
+	{
+		tetris = ft_strsplit(lines[i], '\0');
+		node  = fill_node(tetris, (char) i + 65);
+		new = ft_lstnew(node, sizeof(node) + sizeof(tetris));
+		new->next = tmp;
+		tmp = new;
+	}
+	return (NULL);
+}
