6b84e2947885ed20f74a2eea090f0762696349e5
lenox <maxime.kayumba@epitech.eu>
2018-03-29 21:23:19 +0200

    la fonction shift_n renvoi le pseudo table de veriter,(elle fonction bien mais je vais l'ameliore apres)

--- srcs/algorithm/shift_n.c
+++ srcs/algorithm/shift_n.c
@@ -5,24 +5,51 @@ char			**shift_n(char **tab, int x, int y)
 {
 	int		i;
 	int		j;
+	int		k;
 
 
 	j = -1;
 	i = 0;
 	while (++j < x)
-		tab[i][j] = j + 48;
+		tab[i][j] = j + 65;
 	while (tab[++i])
 	{
  		j = 0;
 		tab[i][x - 1] = tab[i - 1][j];
 		while (++j < x)
 			tab[i][j - 1] = tab[i - 1][j];
-		if (i == y)
-		{
-			tab[i][0] = tab[i - 1][j - 1];
-			tab[i][1] = tab[i - 1][j - 2];
-			tab[i][2] = tab[i - 1][j - 3];
-		}
+		k = -1;
+		while(y == i && --j != -1)
+			tab[i][++k] = tab[i - 1][j];
 	}
 	return (tab);
 }
+
+
+/* char			**shift_n(char **tab, int x, int y) */
+/* { */
+/* 	int		i; */
+/* 	int		j; */
+/* 	int		k; */
+
+
+/* 	j = -1; */
+/* 	i = 0; */
+/* 	while (++j < x) */
+/* 		tab[i][j] = j + 48; */
+/* 	while (tab[++i]) */
+/* 	{ */
+/*  		j = -1; */
+/* 		//tab[i][x - 1] = tab[i - 1][j]; */
+/* 		k = -1; */
+/* 		while (k < x && ++j) */
+/* 		{ */
+/* 			tab[i][++k] = tab[i - 1][x - j]; */
+/* 			printf("j:\t%d\n", j); */
+/* 		} */
+/* 		k = -1; */
+/* 		while(y == i && --j != -1) */
+/* 			tab[i][++k] = tab[i - 1][j]; */
+/* 	} */
+/* 	return (tab); */
+/* } */
