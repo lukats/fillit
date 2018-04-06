33aec3ccbd89821412f82c8a556261af2c615507
lukats <tsiaraso@gmail.com>
2018-04-06 14:51:41 +0200

    fix errors

--- include/fillit.h
+++ include/fillit.h
@@ -20,13 +20,14 @@ char					*replace_char(char *str, char c);
 t_list					*put_tetriminos_list(char **lines);
 char					*get_tetriminos(char *file);
 t_tetris				*fill_node(char **tetris, char lettre);
-char					**shift_n(char **tab, int x, int y);
-int						ft_tetris_mask(char **t);
+char					**ft_tab_truth(int x, int y);
+int						ft_tetris_mask(t_list *t);
 char					**ft_new_tetris(char **tetris, t_tetris *src);
 void					ft_size_tetriminos(t_tetris *src);
 int						ft_check_tetri(char **minos);
 U_INT					ft_nlines(U_INT x);
 int						get_id_tetris(char **lines);
-int		*tab_id_tetris(t_list *list);
-char	**tab_tmp(t_list *list);
+int						*tab_id_tetris(t_list *list);
+char					**tab_tmp(t_list *list);
+
 #endif
